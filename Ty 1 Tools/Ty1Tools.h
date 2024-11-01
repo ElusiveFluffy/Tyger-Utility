45cb7ce8b9d3637f8b2f1e8e91af6112d488298e
ElusiveFluffy <Freya8822@hotmail.com>
2024-11-01 12:34:03 +1030

    Change on init function in the header

--- Ty 1 Tools/Ty1Tools.h
+++ Ty 1 Tools/Ty1Tools.h
@@ -3,7 +3,7 @@ class Ty1Tools
 {
 public:
 	static void TickBeforeGame(float deltaSeconds);
-	static void CheckIfGameFinishInit();
+	static void OnTyInit();
 	static void SaveSettings();
 	static void LoadSettings();
 };
