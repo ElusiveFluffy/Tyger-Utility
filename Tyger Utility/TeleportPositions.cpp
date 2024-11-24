a439043f0081ff8f7cd44e0de54192a4a83fb4a0
ElusiveFluffy <Freya8822@hotmail.com>
2024-11-24 22:01:00 +1030

    Mention how to cycle through the position saving slots

--- Tyger Utility/TeleportPositions.cpp
+++ Tyger Utility/TeleportPositions.cpp
@@ -99,9 +99,9 @@ void TeleportPositions::TeleportPosDrawUI()
 {
 	ImGui::Spacing();
 	ImGui::SeparatorText("Saved Slots:");
-	ImGui::Text("Current Slot: %d", CurrentSlot);
-	ImGui::Text("(Home to Return to the Level Spawn)");
-	ImGui::Text("(Shift+F4 to Save, F4 to Load)");
+	ImGui::Text("Current Slot: %d ('[' and ']' to Cycle Slots)", CurrentSlot);
+	ImGui::Text("('Home' to Return to the Level Spawn)");
+	ImGui::Text("('Shift+F4' to Save, 'F4' to Load)");
 	ImGui::Separator();
 
 	bool noSlotsSaved = true;
