faf65c2053052a34b9205b43093318bc99475da2
Kana <102465188+ElusiveFluffy@users.noreply.github.com>
2025-06-10 13:45:59 +0930

    Show the opal/charge bite counter when changing charge bite count

--- Tyger Utility/MiscGUI.cpp
+++ Tyger Utility/MiscGUI.cpp
@@ -8,7 +8,14 @@
 void MiscGUI::MiscDrawUI()
 {
 	if (ImGui::InputScalar("Charge Bite Count", ImGuiDataType_U8, &ChargeBiteCount, &GUI::IntStepAmount))
+	{
 		*TyAttributes::GetChargeBiteOpalCounterPtr() = ChargeBiteCount * 100;
+
+		//Show the opal counter
+		*(int*)(TyBaseAddress + 0x265488) = 1;
+		//Opal counter display time
+		*(int*)(TyBaseAddress + 0x26548C) = 160;
+	}
 
 	ImGui::Checkbox("Ty Invincibility", TyState::TyInvincibility());
 	GUI::AddToolTip("Only affects Ty, doesn't affect Bull");
